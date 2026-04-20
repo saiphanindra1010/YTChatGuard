76f7d480a5ec72b7cd27b35746be4f0c53cfde31
saiphanindra1010ddsd <saiphanindra1010@gmail.com>
2026-04-21 00:36:07 +0530

    addind better styling

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/public/**/*.html'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    // Order matches in-app theme picker (DaisyUI built-ins; newer names like silk/abyss need DaisyUI 5+).
+    themes: [
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset'
+    ],
+    darkTheme: 'sunset',
+    logs: false
+  }
+};
